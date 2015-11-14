6703e831484564e5587280627ee98ad0f1d6a40e
Siggi <Siggi@SomePC>
2015-11-14 12:46:49 +0100

    edit Offer HTML

--- edit_Offer_Orga_HTML.php
+++ edit_Offer_Orga_HTML.php
@@ -26,43 +26,69 @@ function selectCountryDropbox(){
 
 }
 
-function selectCountryDropbox(){
-
-    try{
-        global $db;
-        
-        $statementStartC = "Select countryname from countries";
-
-        foreach ( $db->query($statementStartC) as $row){
-
-            echo "<option value=$row[countryname]>$row[countryname]</option>\n";
-        }
-
-        } catch (Exception $ex) {
-
-        }
-
-
-}
-
-function getCountry(){
+function checkBoxProducts($offer_id){
     
     global $db;
     
     try{
-     $statementProd = "Select countryname from organisation_offer"
-             . "JOIN ";
+        
+     $checked = FALSE;
+     
+     $statementProd = "Select * from products";
 
-     foreach ( $db->query($statementProd) as $row){
-
-         echo "<p><input type=\"checkbox\" name=\"productChoice[]\" value=$row[productname] id=\"check1\" checked> $row[productname]</p>\n";
-      
+     $statementJoin = "SELECT * FROM productsorgajoin WHERE ID_organisationOffer = $offer_id";
+     
+     foreach ( $db->query($statementProd) as $row1){
+         
+         $checked = FALSE;
+         
+         foreach( $db->query($statementJoin) as $row2){
+            if($row1['ID'] === $row2['ID_product'])
+               $checked = True; 
+         }
+         if($checked)
+            echo "<p><input type=\"checkbox\" name=\"productChoice[]\" value=$row1[productname] id=\"check1\" checked> $row1[productname]</p>\n";
+         else
+             echo "<p><input type=\"checkbox\" name=\"productChoice[]\" value=$row1[productname] id=\"check1\"> $row1[productname]</p>\n";
      }
 
     } catch (Exception $ex) {
 
     }
     
+}
+
+function getCountry($id){
+    
+    global $db;
+    
+    try{
+     $statementProd = "Select countryName from countries "
+             . "WHERE id = $id ";
+
+     foreach ( $db->query($statementProd) as $row){
+
+         return $row['countryName'];
+      
+     }
+
+    } catch (Exception $ex) {
+        
+        echo "ERROR";
+
+    }  
+}
+
+function getColumnData($id, $column){
+    
+    global $db;
+    
+    $statement = "SELECT $column FROM organisation_offer WHERE id = $id";
+    
+    foreach ($db->query($statement) as $row){
+        
+        return $row[$column];
+    }
 }
 
 
@@ -94,18 +120,31 @@ function test_input($data) {
 <!Doctype HTML>
 <html>
     
+    <?php
+    $id = $_POST['offerID'];
+    $id = 2;
+    ?>
+    
     <body>    
     
         <form onSubmit="return" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 
-            <p>Ihr Organame: <input type="text" name="orga" /></p>
+            <p>Ihr Organame: <input type="text" name="orga" value=
+                                <?php
+                                    echo getColumnData($id, 'organisation');
+                                ?>>
+            </p>
+            
             <p>Das Startland: 
             
                 <select name="StartCountry">
-                    <option value="--">
+                    <option value=
+                        <?php
+                            echo getCountry(getColumnData($id, 'startCountry'));
+                        ?>
+                    >
                     <?php
-                    
-                    echo "Countryname out of Database"
+                        echo getCountry(getColumnData($id, 'startCountry'));
                     ?>
                     </option>
                     <?php
@@ -115,11 +154,25 @@ function test_input($data) {
             </p>  
                 
             <p>gewünschtes Land nicht gelistet: <input type="text" name="newstartCountry" /></p>
-            <p>Das Startdorf: <input type="text" name="startVillage" /></p>
+            
+            <p>Das Startdorf: <input type="text" name="startVillage" value=
+                <?php
+                    echo getColumnData($id, 'startVillage');
+                ?>>
+            </p>
+            
             <p>Das Zielland:  
             
                 <select name="EndCountry">
-                    <option value="--">--</option>
+                    <option value=
+                        <?php
+                            echo getCountry(getColumnData($id, 'destinationCountry'));
+                        ?>
+                    >
+                        <?php
+                            echo getCountry(getColumnData($id, 'destinationCountry'));
+                        ?>
+                    </option>
                     <?php
                         selectCountryDropbox();
                    ?> 
@@ -127,13 +180,28 @@ function test_input($data) {
             </p>
             
             <p>gewünschtes Land nicht gelistet: <input type="text" name="newdestCountry" /></p>
-            <p>Das Zieldorf: <input type="text" name="destVillage" /></p>
-            <p>Das Startdatum: <input type="date" name="startDate" /></p>
-            <p>Das Enddatum: <input type="date" name="endDate" /></p>
+            
+            <p>Das Zieldorf: <input type="text" name="destVillage" value=
+                <?php
+                    echo getColumnData($id, 'destinationVillage');
+                ?>>
+            </p>
+            
+            <p>Das Startdatum: <input type="date" name="startDate" value=
+                <?php
+                    echo getColumnData($id, 'startDate');
+                ?>>
+            </p>
+            
+            <p>Das Enddatum: <input type="date" name="endDate" value=
+                <?php
+                    echo getColumnData($id, 'endDate');
+                ?>>
+            </p>
             <p>Welches Produkt: 
                     
                     <?php      
-                        checkBoxProducts();
+                        checkBoxProducts($id);
                     ?>
             <p><input type="submit" /></p>
         </form>
