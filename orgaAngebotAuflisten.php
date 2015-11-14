f4e28fcab6285aac790363a549183b07daaedcfe
Siggi <Siggi@SomePC>
2015-11-14 10:48:27 +0100

    Minor Bugfix (wrong tablename used)

--- orgaAngebotAuflisten.php
+++ orgaAngebotAuflisten.php
@@ -20,10 +20,10 @@
                                                   array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
                   $statement = 'SELECT oo.id, oo.organisation, c1.countryName as startCountry, oo.startVillage, c2.countryName as destCountry, oo.destinationVillage, '
-                          . 'oo.startDate FROM deliverer_offer oo '
+                          . 'oo.startDate FROM organisation_offer oo '
                           . 'join countries c1 on oo.startCountry = c1.id '
                           . 'join countries c2 on oo.destinationCountry = c2.ID';
-                  foreach($db->query('SELECT * FROM organisation_offer') as $row)
+                  foreach($db->query($statement) as $row)
                   /*while($row = mysql_fetch_array($result))*/
                   {   //Creates a loop to loop through results
                           echo "<tr>\n";
@@ -33,7 +33,7 @@
                           echo "<td>" . htmlspecialchars($row['startVillage']) . "</td>\n";
                           echo "<td>" . htmlspecialchars($row['destCountry']) . "</td>\n";
                           echo "<td>" . htmlspecialchars($row['destinationVillage']) . "</td>\n";
-                          echo "<td>" . htmlspecialchars($row['startdate']) . "</td>\n";
+                          echo "<td>" . htmlspecialchars($row['startDate']) . "</td>\n";
                           echo "<td>" . "<a href=\"mailto.html/mail?\">kontaktieren </a></td>\n";
                           echo "</tr>";  //$row['index'] the index here is a field name
                   }
