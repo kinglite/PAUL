8bc04e0b0da49d107fec0368e09c16183ebe44b4
Siggi <Siggi@SomePC.hfu-open.fu.hs-furtwangen.de>
2015-11-20 12:45:00 +0100

    Leere Eingabe abgefangen und Fehlermeldungen hinzugefügt

--- Siggi/create_Organisation_Offer_HTML.php
+++ Siggi/create_Organisation_Offer_HTML.php
@@ -9,6 +9,8 @@ $db = new PDO('mysql:host=localhost;
 include 'Angebot_erstellen.php';
 include './Orga_Offer_HTML_functions.php';
 
+$orgaErr = $startCErr = $startVErr = $destCErr = $destVErr = $startDateErr = $endDateErr = $productErr = "";
+
 function checkBoxProducts(){
     
     global $db;
@@ -33,12 +35,91 @@ function checkBoxProducts(){
 <!Doctype HTML>
 <html>
     
-    <body>    
+    <head>
+        <style>
+            .error {color: #FF0000;}
+        </style>
+    </head>
+    
+    <body> 
+        <?php 
+        $postOK = true;
+        
+            if ($_SERVER["REQUEST_METHOD"] == "POST"){
+                if(empty($_POST["orga"])){
+                    $postOK = false;
+                    $orgaErr = "Bitte Namen eingeben";
+                }
+                else
+                    $name = test_input($_POST["orga"]);
+                
+               if(!empty($_POST["newstartCountry"]))
+                   $startCountry = test_input($_POST["newstartCountry"]);
+               else{
+                   if( $_POST["StartCountry"] == "--"){
+                     $startCErr = "Bitte Land auswählen oder eigenes Land eingeben"; 
+                     $postOK = false;
+                   }
+                   else
+                     $startCountry = test_input($_POST["StartCountry"]);
+               }
+               
+               if(empty($_POST["startVillage"])){
+                  $startVErr = "Bitte Dorf eingeben"; 
+                  $postOK = false; 
+               }
+               else
+                    $startVillage = test_input($_POST["startVillage"]);
+               
+               if(!empty($_POST["newdestCountry"]))
+                    $destCountry = test_input($_POST["newdestCountry"]);
+               else{
+                   if( $_POST["EndCountry"] == "--"){
+                     $destCErr = "Bitte Land auswählen oder eigenes Land eingeben"; 
+                     $postOK = false;
+                   }
+                    $destCountry = test_input($_POST["EndCountry"]);
+               }
+               
+               if(empty($_POST["destVillage"])){
+                  $destVErr = "Bitte Dorf eingeben"; 
+                  $postOK = false;
+               }
+               else
+                    $destVillage = test_input($_POST["destVillage"]);
+               
+               if(empty($_POST["startDate"])){
+                  $startDateErr = "Bitte Datum eingeben"; 
+                  $postOK = false; 
+               }
+               else
+                    $startDate = test_input($_POST["startDate"]);
+               
+               if(empty($_POST["endDate"])){
+                    $endDateErr = "Bitte Dorf eingeben"; 
+                    $postOK = false;
+               }
+               else
+                    $endDate = test_input($_POST["endDate"]);
+               
+               if(empty($_POST["productChoice"])){
+                   $productErr = "Bitte mind. ein Produkt auswählen"; 
+                    $postOK = false;
+               }
+               else
+                   $products = ($_POST["productChoice"]);
+               
+               if($postOK)
+                    create_Organisation_Offer($name, $startCountry, $startVillage, $destCountry, $destVillage, $startDate, $endDate, $products);
+            }
+        ?>
     
         <form onSubmit="return" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 
-            <p>Ihr Organame: <input type="text" name="orga" /></p>
-            <p>Das Startland: 
+            <p>Ihr Organame*: <input type="text" name="orga" />
+                <span class="error"> <?php echo $orgaErr;?></span>
+            </p>
+            <p>Das Startland*: 
             
                 <select name="StartCountry">
                     <option value="--">--</option>
@@ -46,11 +127,14 @@ function checkBoxProducts(){
                         selectCountryDropbox();
                    ?> 
                 </select> 
+                <span class="error"> <?php echo $startCErr;?></span>
             </p>  
                 
             <p>gewünschtes Land nicht gelistet: <input type="text" name="newstartCountry" /></p>
-            <p>Das Startdorf: <input type="text" name="startVillage" /></p>
-            <p>Das Zielland:  
+            <p>Das Startdorf*: <input type="text" name="startVillage" />
+                <span class="error"> <?php echo $startVErr;?></span>
+            </p>
+            <p>Das Zielland*:  
             
                 <select name="EndCountry">
                     <option value="--">--</option>
@@ -58,41 +142,27 @@ function checkBoxProducts(){
                         selectCountryDropbox();
                    ?> 
                 </select> 
+                <span class="error"> <?php echo $destCErr;?></span>
             </p>
             
             <p>gewünschtes Land nicht gelistet: <input type="text" name="newdestCountry" /></p>
-            <p>Das Zieldorf: <input type="text" name="destVillage" /></p>
-            <p>Das Startdatum: <input type="date" name="startDate" /></p>
-            <p>Das Enddatum: <input type="date" name="endDate" /></p>
+            <p>Das Zieldorf*: <input type="text" name="destVillage" />
+                <span class="error"> <?php echo $destVErr;?></span>
+            </p>
+            <p>Das Startdatum*: <input type="date" name="startDate" />
+                <span class="error"> <?php echo $startDateErr;?></span>
+            </p>
+            <p>Das Enddatum*: <input type="date" name="endDate" />
+                <span class="error"> <?php echo $endDateErr;?></span>
+            </p>
             <p>Welches Produkt: 
                     
                     <?php      
                         checkBoxProducts();
                     ?>
+                <span class="error"> <?php echo $productErr;?></span>
             <p><input type="submit" /></p>
         </form>
-        
-        <?php 
-            if ($_SERVER["REQUEST_METHOD"] == "POST"){
-               $name = test_input($_POST["orga"]);
-               if($_POST["newstartCountry"] != ""){
-                    $startCountry = test_input($_POST["newstartCountry"]);
-               }
-               else
-                    $startCountry = test_input($_POST["StartCountry"]);
-               $startVillage = test_input($_POST["startVillage"]);
-               if($_POST["newdestCountry"] != "")
-                    $destCountry = test_input($_POST["newdestCountry"]);
-               else
-                    $destCountry = test_input($_POST["EndCountry"]);
-               $destVillage = test_input($_POST["destVillage"]);
-               $startDate = test_input($_POST["startDate"]);
-               $endDate = test_input($_POST["endDate"]);
-               $products = ($_POST["productChoice"]);
-               
-               create_Organisation_Offer($name, $startCountry, $startVillage, $destCountry, $destVillage, $startDate, $endDate, $products);
-            }
-        ?>
     </body>
     
 </html>
